a028cad0728042ef227961628d6418b96a263bdb
Sergi Juanola <korcholis@gmail.com>
2017-03-15 09:23:45 +0100

    starting to unit test

---
+++ test/DwarfTest.php
@@ -0,0 +1,39 @@
+<?php
+require(__DIR__ . '/../src/Dwarf.php');
+
+use Dwarf\Dwarf;
+
+final class DwarfTest extends PHPUnit\Framework\TestCase {
+
+  private $config = [
+    'rootPath' => __DIR__
+  ];
+
+  private $dwarf = null;
+
+  public function setUp() {
+    $this->dwarf = new Dwarf($this->config);
+  }
+
+  public function testCanBeInstanced() {
+    $this->assertInstanceOf(
+      Dwarf::class,
+      $this->dwarf
+    );
+  }
+
+  public function testPaths() {
+    $this->assertEquals(
+      realpath(__DIR__),
+      $this->dwarf->getRootPath()
+    );
+    $this->assertEquals(
+      realpath(__DIR__ . DIRECTORY_SEPARATOR . 'app'),
+      $this->dwarf->getAppPath()
+    );
+    $this->assertEquals(
+      realpath(__DIR__ . DIRECTORY_SEPARATOR . 'templates'),
+      $this->dwarf->getTemplatePath()
+    );
+  }
+}
